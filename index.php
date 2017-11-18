6ae18d4d296c0ef703000d357ddc57be3db3b1de
Unknown <nsehall.coding@gmail.com>
2017-11-18 12:54:57 +1000

    Base Page Setup

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Next 5 Web App</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div id="Container">
+		<div id="Content">
+			<div id="Times" class="Info">
+				<h1 id="Time1">Times</h1>
+				<h1 id="Time2">Times</h1>
+				<h1 id="Time3">Times</h1>
+				<h1 id="Time4">Times</h1>
+				<h1 id="Time5">Times</h1>
+			</div>
+			<div id="Types" class="Info">
+				<h1 id="Type1">Types</h1>
+				<h1 id="Type2">Types</h1>
+				<h1 id="Type3">Types</h1>
+				<h1 id="Type4">Types</h1>
+				<h1 id="Type5">Types</h1>
+			</div>
+			<div id="Names" class="Info">
+				<h1 id="Name1">Names</h1>
+				<h1 id="Name2">Names</h1>
+				<h1 id="Name3">Names</h1>
+				<h1 id="Name4">Names</h1>
+				<h1 id="Name5">Names</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
